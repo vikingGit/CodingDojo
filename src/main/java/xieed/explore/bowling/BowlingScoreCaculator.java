e82de670949ca20538d5e4ba1c9db732a84e5481
xieed <eddy.xie@oocl.com>
2019-01-26 21:21:48 +0800

    get one turn score for strike and spare

--- src/main/java/xieed/explore/bowling/BowlingScoreCaculator.java
+++ src/main/java/xieed/explore/bowling/BowlingScoreCaculator.java
@@ -2,7 +2,15 @@ package xieed.explore.bowling;
 
 class BowlingScoreCaculator {
 
-  int caculateScoreInOneTurn(int firstTryBalls, int secondTryBalls) {
-    return firstTryBalls + secondTryBalls;
+  private final static int ALL_BALL_QUANTITY = 10;
+
+  String caculateScoreInOneTurn(int firstTryBalls, int secondTryBalls) {
+    if(firstTryBalls == ALL_BALL_QUANTITY){
+      return "STRIKE";
+    }
+    if((firstTryBalls + secondTryBalls) == ALL_BALL_QUANTITY){
+      return "SPARE";
+    }
+    return String.valueOf(firstTryBalls + secondTryBalls);
   }
 }
--- src/test/java/xieed/explore/bowling/BowlingScoreCaculatorTest.java
+++ src/test/java/xieed/explore/bowling/BowlingScoreCaculatorTest.java
@@ -7,12 +7,23 @@ public class BowlingScoreCaculatorTest {
   @Test
   public void should_return_7_when_7_ball_knock_down_given_one_try() throws Exception {
     BowlingScoreCaculator caculator = new BowlingScoreCaculator();
-    Assert.assertSame(7, caculator.caculateScoreInOneTurn(7, 0));
+    Assert.assertEquals("7", caculator.caculateScoreInOneTurn(7, 0));
   }
 
   @Test
   public void should_return_9_when_4_and_5_balls_knock_down_separately_given_two_try() throws Exception {
     BowlingScoreCaculator caculator = new BowlingScoreCaculator();
-    Assert.assertEquals(9, caculator.caculateScoreInOneTurn(4, 5));
+    Assert.assertEquals("9", caculator.caculateScoreInOneTurn(4, 5));
+  }
+
+  @Test
+  public void should_return_strike_when_first_try_knock_down_10_balls_given_one_turn() throws Exception {
+    BowlingScoreCaculator caculator = new BowlingScoreCaculator();
+    Assert.assertEquals("STRIKE", caculator.caculateScoreInOneTurn(10, 0));
+  }
+  @Test
+  public void should_return_spare_when_two_tries_knock_down_10_balls_given_one_turn() throws Exception {
+    BowlingScoreCaculator caculator = new BowlingScoreCaculator();
+    Assert.assertEquals("SPARE", caculator.caculateScoreInOneTurn(4, 6));
   }
 }
