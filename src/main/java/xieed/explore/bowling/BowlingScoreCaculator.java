abe9424e770f11ae31cd90b84db78118f76a517c
xieed <eddy.xie@oocl.com>
2019-01-26 15:45:24 +0800

    refactor: add package and introduce "turn"

---
+++ src/main/java/xieed/explore/bowling/BowlingScoreCaculator.java
@@ -0,0 +1,8 @@
+package xieed.explore.bowling;
+
+class BowlingScoreCaculator {
+
+  int caculateScoreInOneTurn(int firstTryBalls, int secondTryBalls) {
+    return firstTryBalls + secondTryBalls;
+  }
+}
--- src/test/java/xieed/explore/bowling/BowlingScoreCaculatorTest.java
+++ src/test/java/xieed/explore/bowling/BowlingScoreCaculatorTest.java
@@ -1,17 +1,18 @@
+package xieed.explore.bowling;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class BowlingScoreCaculatorTest {
-
   @Test
   public void should_return_7_when_7_ball_knock_down_given_one_try() throws Exception {
     BowlingScoreCaculator caculator = new BowlingScoreCaculator();
-    Assert.assertSame(7, caculator.getScore(7));
+    Assert.assertSame(7, caculator.caculateScoreInOneTurn(7, 0));
   }
 
   @Test
   public void should_return_9_when_4_and_5_balls_knock_down_separately_given_two_try() throws Exception {
     BowlingScoreCaculator caculator = new BowlingScoreCaculator();
-    Assert.assertEquals(9, caculator.getScore(4, 5));
+    Assert.assertEquals(9, caculator.caculateScoreInOneTurn(4, 5));
   }
 }
